7f2d4f96567a686e57a642ecf2635e8620c466d3
Ruslan <cut6.cult6.cult6@gmail.com>
2020-10-12 23:50:55 +0300

    create blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Post(models.Model):
+    Post_title = models.CharField(max_length=50)
+    Post_pub_date = models.DateTimeField(default=now)
+    Post_text = models.TextField(max_length=300)
+    Post_image = models.ImageField(upload_to='event_images/')
--- events/models.py
+++ events/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Events(models.Model):
+    Event_image = models.ImageField(upload_to='event_images/')
+    Event_text = models.CharField(max_length=200)
