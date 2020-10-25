b8f8a0d9112d0c828cf5a3ca54c37b06107dc1e4
Ruslan <cut6.cult6.cult6@gmail.com>
2020-10-26 00:37:07 +0300

    Add function, witch can print only 70 symbols of blog's text

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,10 @@ class Post(models.Model):
     Post_pub_date = models.DateTimeField(default=now)
     Post_text = models.TextField(max_length=300)
     Post_image = models.ImageField(upload_to='event_images/')
+
+    def get_summary(self):
+        text = self.Post_text
+        if len(text) > 70:
+            return self.Post_text[:60] + "..."
+        else:
+            return self.Post_text
